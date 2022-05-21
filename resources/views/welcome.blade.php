a97b550acb585ba671685719840de39c3054a0d5
joseluis <joseluis.universidad2020@gmail.com>
2022-05-21 19:57:07 -0400

    segundo commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,8 +38,8 @@
             @endif
 
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
-                <!-- IMAGEN DE MARIO -->
-                <img src="imagenes/mario.png" alt="Mario riendo" width="200" height="300" >
+                <!-- IMAGEN DE UN TRUFI -->
+                <img src="imagenes/trufi.png" alt="es la imagen de una cafe" width="540" height="340" >
 
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
                     <div class="grid grid-cols-1 md:grid-cols-2">
